98960383bea5a96f85314f2da80b31fe694a4eaf
GGGWX <gggwx6198@yahoo.com>
2018-12-26 00:10:09 +0800

    delete useless comments

--- dyxCrawler.py
+++ dyxCrawler.py
@@ -1,12 +1,8 @@
-#from requests_html import HTMLSession
 import time
-import re
 import requests
 import csv, codecs
 import lxml
 from lxml import etree
-from lxml import html
-import pymysql
 
 f = open("../dxyhl.csv", "w+", encoding="utf-8-sig")
 
@@ -24,31 +20,17 @@ urls=[]
 writer = csv.writer(f)
 writer.writerow(('title', 'author', 'when', 'reply', 'click', 'rate', 'content'))
 
+# know nothing about python crawler, but chrome is a good tool to analyze xml
+# and fortunately i learned xml, so use xml to write a crawler
+# (regex looks like disaster). I have to say dxy is a website with weird structure
+# spend much time on it, finally it worked fine. I hope that if someday i become a
+# front-end programmer, the structure must be limpid for others to read.
 
 def get_info(url):
     selector = etree.HTML(get_html(url))
-
+# maybe there should exist a try-catch block, i dismiss it
     for i in range(1, 36):
-        # title = selector.xpath(fixed_path + '[' + str(i) + ']/td[2]/a/text()')
-        # if title:
-        #     title = title[0]
-        # else:
-        #     continue
-        # title = selector.xpath(fixed_path + '[' + str(i) + ']/td[2]/a/text()')
-        # if not title:
-        #     new_title = selector.xpath(fixed_path + '[' + str(i) + ']/td[2]/a[2]/text()')
-        #     title = title[0]
-        # else:
-        #     title = title[0]
         title = selector.xpath(fixed_path + '[' + str(i) + ']/td[2]/a[last()]/text()')[0]
-        print(title)
-        # attr = selector.xpath(fixed_path + '[' +str(i) + ']/td[2]/a/@class/text()')
-        # if attr:
-        #     title = selector.xpath(fixed_path + '[' + str(i) + ']/td[2]/a[2]/text()')[0]
-        # else:
-        #     title = selector.xpath(fixed_path + '[' + str(i) + ']/td[2]/a/text()')[0]
-        # //*[@id="col-1"]/table[3]/tbody/tr[25]/td[2]/a[1] 
-        # //*[@id="col-1"]/table[3]/tbody/tr[25]/td[2]/a[2]
         # print(title) 
         author = selector.xpath(fixed_path + '[' + str(i) + ']/td[3]/a/text()')[0]
         # print(author)
@@ -70,7 +52,7 @@ def get_info(url):
 
 def get_content(url):
     selector = etree.HTML(get_html(url))
-
+# this part may be optimized, and most of the posts have no reply, so only grab the main floor
     first_floors = selector.xpath(' \
     //*[@id="post_1"]/table/tbody/tr/td[2]/div[2]/div[1]/table/tbody/tr/td//p/text() | \
     //*[@id="post_1"]/table/tbody/tr/td[2]/div[2]/div[1]/table/tbody/tr/td//p//a/text() | \
@@ -78,46 +60,15 @@ def get_content(url):
     //*[@id="post_1"]/table/tbody/tr/td[2]/div[2]/div[1]/table/tbody/tr/td//text() | \
     //*[@id="post_1"]/table/tbody/tr/td[2]/div[2]/div[2]/table/tbody/tr/td//text() ')
     first_floors2 = [str(i) for i in first_floors]
-    first_floors3 = ''.join(first_floors2)
-    # print(first_floors3)
-    return first_floors3
+    first_floor = ''.join(first_floors2)
+    # print(first_floor)
+    return first_floor
 
-#max_page = int(etree.HTML(testHTML).xpath('//*[@id="col-1"]/div/div[1]/div[1]/a[4]/text()')[0])
+max_page = int(etree.HTML(testHTML).xpath('//*[@id="col-1"]/div/div[1]/div[1]/a[4]/text()')[0])
 #max pages the bbs have
 
-# //*[@id="post_1"]/table/tbody/tr/td[2]/div[2]/div[1]/table/tbody/tr/td
-# //*[@id="post_1"]/table/tbody/tr/td[2]/div[2]/div[1]/table/tbody/tr/td/text()
-
-
-
 if __name__ == "__main__":
-    
-    # j = 2 #first page
-    # url = basicURL + '&tpg=' + str(j)
-    # get_info(url)
-    # print(urls[0])
-    # j = 2
-    # while(j <= 10):
-    #     url = basicURL + '&tpg=' + str(j)
-    #     get_info(url)
-    #     j = j + 1
-    # for n in range(122,124):
-    #     url = basicURL + '&tpg=' + str(n)
-    #     get_info(url)
-    get_info("http://www.dxy.cn/bbs/board/106?order=1&tpg=177")
-    
-
-    
-
-
-
-
-#//*[@id="col-1"]/div/div[1]/div[1]/a[4]        all pages
-#//*[@id="col-1"]/table[3]/tbody/tr[1]          odd...
-#//*[@id="col-1"]/table[3]/tbody/tr[2]          even...
-#//*[@id="col-1"]/table[3]/tbody/tr[1]/td[2]/a  title
-#//*[@id="col-1"]/table[3]/tbody/tr[1]/td[3]/a  author
-#//*[@id="col-1"]/table[3]/tbody/tr[1]/td[3]/em when
-#//*[@id="col-1"]/table[3]/tbody/tr[1]/td[4]/a  reply
-#//*[@id="col-1"]/table[3]/tbody/tr[1]/td[4]/em click
-#//*[@id="col-1"]/table[3]/tbody/tr[1]/td[5]/em lastReply
+    for n in range(1, max_page + 1):
+        url = basicURL + '&tpg=' + str(n)
+        get_info(url)
+        time.sleep(1)# whatever, connot afford a proxy ip
